da6fe589b47ba65c0e8598f3670d2b18ba3c0475
alex-levin <alxlevin@outlook.com>
2018-11-11 14:35:52 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { WeatherService, WeatherInterface } from './services/weather.service';
-import { GeocodeService, GeocodeInterface } from './services/geocode.service';
+import { GeocodeService } from './services/geocode.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,14 @@ export class AppComponent {
   title = 'weatherspi';
 
   constructor(service: GeocodeService) {
-    service.getCoordinatesByZipCode('01760');
+    service.getCoordinatesByZipCode('01760').subscribe(data => {
+      // casting data to any to avoid resourceSets property does not exist on type Object
+      let coordinatesArr: Array<string> = (data as any).resourceSets[0].resources[0].point.coordinates;
+      let lat: string = coordinatesArr[0];
+      let lon: string = coordinatesArr[1];
+      // prints 42.2846603393555, -71.3477401733398
+      console.log(lat + ', ' + lon);
+    });
   }
 }
 
--- src/app/services/geocode.service.ts
+++ src/app/services/geocode.service.ts
@@ -4,22 +4,13 @@ import { HttpClient } from '@angular/common/http';
 let key: string = 'AuocNZZGFHV1k0cPvoe0isu3PUJWtKuATK6u9kUImXkzyBsC9p3yeMUA-043beei';
 let url: string = 'http://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US';
 
-export interface GeocodeInterface {
-    lat: string;
-    lon: string;
-}
 @Injectable()
 export class GeocodeService {
   constructor(private http: HttpClient) { }
 
   getCoordinatesByZipCode(zipCode: string) {
     url = url + '&postalCode=' + zipCode + '&key=' + key;
-    return this.http.get(url).subscribe(data => {
-        console.log(data as any);
-        // casting data to any to avoid resourceSets property does not exist on type Object
-        // prints Array [ 42.2846603393555, -71.3477401733398 ]
-        console.log((data as any).resourceSets[0].resources[0].point.coordinates);
-    });
+    return this.http.get(url);
   }
 
   getCoordinatesByLocation(city: string, state: string) {
